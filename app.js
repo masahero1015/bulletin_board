e22d43a4b3f946e33d3555ebcb9d134c5d34c5d1
masahiro1015 <masahiropper20021015@gmail.com>
2024-06-25 16:26:16 +0900

    make editbtn and deletebtn function

--- app.js
+++ app.js
@@ -14,25 +14,63 @@ form.addEventListener("submit", (event) => {
         newtweet.className = 'first';
         zero++;
     }
-    newtweet.innerHTML = `${username.value}<br>${centence.value}`;
+    newtweet.innerHTML = `名前：${username.value}<br>${centence.value}`;
     posting.appendChild(newtweet);
 
-    const edietBtn = document.createElement('button');
-    edietBtn.innerHTML = '編集';
-    edietBtn.className = 'editBtn';
-    posting.appendChild(edietBtn);
-    edietBtn.addEventListener('click', (e) => {
+    const editBtn = document.createElement('button');
+    editBtn.innerHTML = '編集';
+    editBtn.className = 'editBtn';
+    posting.appendChild(editBtn);
+    editBtn.addEventListener('click', (e) => {
+        const detail = e.target.previousElementSibling;
+        const nameSize = detail.innerHTML.indexOf('<br>');
+        const sentence = detail.innerHTML;
+        if (detail.innerHTML.includes('<br>')) {
+            detail.innerHTML = sentence.substring(0, nameSize + 4);
+        }
+        const detailArea = document.createElement('textarea');
+        detailArea.innerHTML = sentence.substring(nameSize + 4);
+        detailArea.className = 'detailArea'
+        e.target.previousElementSibling.insertAdjacentElement('afterend',detailArea);
+
+
+        const editComp = document.createElement('button');
+        editComp.innerHTML = '編集完了';
+        editComp.className = 'editBtn';
+        editComp.style.display = 'block';
+
+        e.target.insertAdjacentElement('afterend', editComp);
+        e.target.style.display = 'none';
+        e.target.nextElementSibling.nextElementSibling.style.display = 'none';
+
+
+        editComp.addEventListener('click', (c) => {
+            const editSentence = c.target.previousElementSibling.previousElementSibling;
+            const list = editSentence.previousElementSibling;
+
+            console.log(editSentence.value);
+            console.log(list.innerHTML);
+
+            let lisComp = list.innerHTML +  editSentence.value;
+            console.log(lisComp);
+
+            list.innerHTML = lisComp;
+
+            e.target.style.display = 'inline';
+            e.target.nextElementSibling.nextElementSibling.style.display = 'inline';
+            editSentence.remove();
+            c.target.remove();
+        })
     });
     
     const deleteBtn = document.createElement('button');
     deleteBtn.innerHTML = '削除';
     posting.appendChild(deleteBtn);
-    deleteBtn.addEventListener('click', (e) => {
-        const deleteLi = e.target.previousElementSibling.previousElementSibling;
-        const deleteEdit = e.target.previousElementSibling;
-        const deleteE = e.target;
+    deleteBtn.addEventListener('click', (d) => {
+        const deleteLi = d.target.previousElementSibling.previousElementSibling;
+        const deleteEdit = d.target.previousElementSibling;
         deleteLi.remove();
         deleteEdit.remove();
-        deleteE.remove();
+        d.target.remove();
     });
 });
