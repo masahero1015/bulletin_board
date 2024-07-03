97c43b7b714f80409108d1d0d59eca7ec4a3eb36
masahiro1015 <masahiropper20021015@gmail.com>
2024-07-03 19:44:45 +0900

    Modify editbutton and edit README

--- README.md
+++ README.md
@@ -1,14 +1,21 @@
 # bulletin_board
 
-ユーザー名と本文を投稿することができます。
-
 投稿前
+
+ユーザー名と本文を記入して投稿ボタンを押すと投稿することができます。
+
 ![bulletin_board1](/image/image1.png)
 
+---
+
 投稿後
+
+投稿した後に編集ボタンを押すと本文を編集することができます。
+また、削除ボタンで投稿を削除できます。
+
 ![bulletin_board2](/image/image2.png)
 
 ## Coding languages
 
 - HTML / CSS
-- Javascript
+- JavaScript
--- app.js
+++ app.js
@@ -1,8 +1,8 @@
 const form = document.querySelector('form');
 const posting = document.querySelector('ul');
 
-
 let zero = 0;
+
 form.addEventListener("submit", (event) => {
     event.preventDefault();
 
@@ -15,17 +15,17 @@ form.addEventListener("submit", (event) => {
         zero++;
     }
 
-    // View posting
+    // 投稿の表示
     newtweet.innerHTML = `名前：${username.value}<br>${centence.value}`;
     posting.appendChild(newtweet);
 
-    // Make editBtn
+    // 編集ボタンを作る
     const editBtn = document.createElement('button');
     editBtn.innerHTML = '編集';
     editBtn.className = 'editBtn';
     posting.appendChild(editBtn);
 
-    // Function when editBtn is pressed
+    // 編集ボタンを押した時
     editBtn.addEventListener('click', (e) => {
         const detail = e.target.previousElementSibling;
         const nameSize = detail.innerHTML.indexOf('<br>');
@@ -36,9 +36,10 @@ form.addEventListener("submit", (event) => {
         const detailArea = document.createElement('textarea');
         detailArea.innerHTML = sentence.substring(nameSize + 4);
         detailArea.className = 'detailArea'
+        detailArea.required = true;
         e.target.previousElementSibling.insertAdjacentElement('afterend', detailArea);
 
-        // Make editComp
+        // 編集完了ボタンを作る
         const editComp = document.createElement('button');
         editComp.innerHTML = '編集完了';
         editComp.className = 'editBtn';
@@ -47,28 +48,32 @@ form.addEventListener("submit", (event) => {
         e.target.insertAdjacentElement('afterend', editComp);
         e.target.style.display = 'none';
         e.target.nextElementSibling.nextElementSibling.style.display = 'none';
-        
-        // Function when editComp is pressed
-        editComp.addEventListener('click', (c) => {
-            const editSentence = c.target.previousElementSibling.previousElementSibling;
+
+        // 編集完了ボタンを押した時
+        editComp.addEventListener('click', () => {
+            const editSentence = e.target.previousElementSibling;
             const list = editSentence.previousElementSibling;
 
-            let lisComp = list.innerHTML + editSentence.value;
-            console.log(lisComp);
+            if (editSentence.value !== '') {
+                let lisComp = list.innerHTML + editSentence.value;
+                editSentence.previousElementSibling.innerHTML = lisComp;
 
-            e.target.style.display = 'inline';
-            e.target.nextElementSibling.nextElementSibling.style.display = 'inline';
-            editSentence.remove();
-            c.target.remove();
+                e.target.style.display = 'inline';
+                e.target.nextElementSibling.nextElementSibling.style.display = 'inline';
+                editSentence.remove();
+                e.target.nextElementSibling.remove();
+            } else {
+                alert('編集内容を記入してください');
+            }
         })
     });
 
-    // Make deleteBtn
+    // 削除ボタンを作る
     const deleteBtn = document.createElement('button');
     deleteBtn.innerHTML = '削除';
     posting.appendChild(deleteBtn);
 
-    // Function when deletebutton is pressed
+    // 削除ボタンを押したとき
     deleteBtn.addEventListener('click', (d) => {
         const lis = document.querySelectorAll('li');
 
@@ -83,14 +88,13 @@ form.addEventListener("submit", (event) => {
         if (lis.length === 1) {
             zero = 0;
         }
-        
-        // Delete Post
+
         targetLis.remove();
         targetEdit.remove();
         d.target.remove();
     });
 
-    // Reset content when posting
+    // 投稿内容をリセット
     username.value = '';
     centence.value = '';
 });
